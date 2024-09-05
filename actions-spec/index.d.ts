6f50bfba5091f9f5f6ddee09bebed0157a3e9282
Ivan <57509528+vaniiiii@users.noreply.github.com>
2024-09-05 18:25:04 +0200

    feat!: Remove constants from Action (#21)

--- actions-spec/index.d.ts
+++ actions-spec/index.d.ts
@@ -16,28 +16,8 @@ export interface Action {
     description: string;
     label: string;
     links?: LinkedAction[];
-    constants?: ActionConstant[];
     error?: ActionError;
 }
-
-/**
- * Action Constant interface
- */
-export interface ActionConstant {
-    label: string;
-    type: ActionConstantType;
-    value: string | number | boolean;
-}
-
-/**
- * Supported types for Constants
- */
-export type ActionConstantType =
-    | 'string'
-    | 'number'
-    | 'boolean'
-    | 'address'
-    | 'timestamp';
 
 /**
  * Input field for an Action
@@ -67,7 +47,6 @@ export interface ActionInputSelectable extends ActionInput {
  */
 export enum InputScope {
     User,
-    Constant,
     Global,
 }
 

fdd8c909052329a0df81b52a24e1ef9a2d16c7a9
Aleksa Uroseivc <aleksa.urosevic0@gmail.com>
2024-09-09 12:44:23 +0200

    chore: Add function signatures for ERC20 and ERC721 methods

--- actions-sdk/src/utils/constants.ts
+++ actions-sdk/src/utils/constants.ts
@@ -6,6 +6,10 @@ interface GlobalInputs {
     [name: string]: string;
 }
 
+interface FuncSignatures {
+    [funcName: string]: string;
+}
+
 export const chainId: ChainIdConfig = {
     ETHEREUM_MAINNET: 1,
     ETHEREUM_GOERLI: 5,
@@ -26,4 +30,27 @@ export const chainId: ChainIdConfig = {
 
 export const globalInputs: GlobalInputs = {
     WALLET_ADDRESS: 'WALLET_ADDRESS',
+};
+
+export const erc20FunctionSignatures: FuncSignatures = {
+    NAME: 'name()',
+    SYMBOL: 'symbol()',
+    DECIMALS: 'decimals()',
+    APPROVE: 'approve(address,uint256)',
+    TRANSFER: 'transfer(address,uint256)',
+    TRANSFER_FROM: 'transferFrom(address,address,uint256)',
+    ALLOWANCE: 'allowance(address,address)',
+    BALANCE_OF: 'balanceOf(address)',
+};
+
+export const erc721FunctionSignatures: FuncSignatures = {
+    OWNER_OF: 'ownerOf(uint256)',
+    BALANCE_OF: 'balanceOf(address)',
+    APPROVE: 'approve(address,uint256)',
+    GET_APPROVED: 'getApproved(uint256)',
+    SET_APPROVAL_FOR_ALL: 'setApprovalForAll(address,bool)',
+    IS_APPROVED_FOR_ALL: 'isApprovedForAll(address)',
+    TRANSFER_FROM: 'transferFrom(address,address,uint256)',
+    SAFE_TRANSFER_FROM: 'safeTransferFrom(address,address,uint256)',
+    SAFE_TRANSFER_FROM_DATA: 'safeTransferFrom(address,address,uint256,bytes)',
 };
